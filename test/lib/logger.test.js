c12cdc95648b7b893fc25244f8ac728218799f4a
Sam Levin <sjl2024@users.noreply.github.com>
2016-07-31 15:54:09 -0400

    BACK-1600: Made logger.js unit tests more comprehensive

--- test/lib/logger.test.js
+++ test/lib/logger.test.js
@@ -15,52 +15,103 @@
 const should = require('should');
 const sinon = require('sinon');
 const uuid = require('node-uuid');
+const stdout = require("test-console").stdout;
 
 const logger = require('../../lib/service/logger');
 
+/**
+ * Helper function to generate sample logging output
+ */
+function generateLogOutput(message, logThreshold, dlcId, backingServerId) {
+  return `{"message":"${message}","level":"${logThreshold}","dlcId":"${dlcId}","backingServerId":"${backingServerId}"}`
+}
+
 describe('sdk logging', () => {
   let logString = '';
+  let dlcId = '';
   let backingServerId = '';
-  beforeEach(() => {
-    logString = uuid.v4();
-    backingServerId = process.env.BACKING_SERVER_ID = uuid.v4();
-  });
+  let sampleMessage = '';
   describe('log levels', () => {
-    it('info', (done) => {
-      const result = `{"message":"${logString}","level":"info","backingServerId":"${backingServerId}"}`;
-      const spy = sinon.spy(logger, 'info');
-      logger.info(logString);
-      spy.getCall(0).args[0].should.eql(logString);
-      spy.returnValues[0].should.eql(result);
-      logger.info.restore();
-      done();
+    beforeEach(() => {
+      logString = uuid.v4();
+      dlcId = process.env.DLC_ID = uuid.v4();
+      backingServerId = process.env.BACKING_SERVER_ID = uuid.v4();
     });
-    it('warn', (done) => {
-      const result = `{"message":"${logString}","level":"warning","backingServerId":"${backingServerId}"}`;
-      const spy = sinon.spy(logger, 'warn');
-      logger.warn(logString);
-      spy.getCall(0).args[0].should.eql(logString);
-      spy.returnValues[0].should.eql(result);
-      logger.warn.restore();
-      done();
+    describe('info', () => {
+      it('successfully logs an \'info\' message', (done) => {
+        sampleMessage = generateLogOutput(logString, 'info', dlcId, backingServerId);
+        const spy = sinon.spy(logger, 'info');
+        logger.info(logString);
+        spy.getCall(0).args[0].should.eql(logString);
+        spy.returnValues[0].should.eql(sampleMessage);
+        logger.info.restore();
+        done();
+      });
+      it('writes the correct output to stdout', (done) => {
+        sampleMessage = generateLogOutput(logString, 'info', dlcId, backingServerId);
+        const inspect = stdout.inspect();
+        logger.info(logString);
+        inspect.restore();
+        inspect.output.should.eql([`${sampleMessage}`]);
+        done()
+      });
     });
-    it('error', (done) => {
-      const result = `{"message":"${logString}","level":"error","backingServerId":"${backingServerId}"}`;
-      const spy = sinon.spy(logger, 'error');
-      logger.error(logString);
-      spy.getCall(0).args[0].should.eql(logString);
-      spy.returnValues[0].should.eql(result);
-      logger.error.restore();
-      done();
+    describe('warn', () => {
+      it('successfully logs an \'warning\' message', (done) => {
+        sampleMessage = generateLogOutput(logString, 'warning', dlcId, backingServerId);
+        const spy = sinon.spy(logger, 'warn');
+        logger.warn(logString);
+        spy.getCall(0).args[0].should.eql(logString);
+        spy.returnValues[0].should.eql(sampleMessage);
+        logger.warn.restore();
+        done();
+      });
+      it('writes the correct output to stdout', (done) => {
+        sampleMessage = generateLogOutput(logString, 'warning', dlcId, backingServerId);
+        const inspect = stdout.inspect();
+        logger.warn(logString);
+        inspect.restore();
+        inspect.output.should.eql([`${sampleMessage}`]);
+        done()
+      });
     });
-    it('fatal', (done) => {
-      const result = `{"message":"${logString}","level":"fatal","backingServerId":"${backingServerId}"}`;
-      const spy = sinon.spy(logger, 'fatal');
-      logger.fatal(logString);
-      spy.getCall(0).args[0].should.eql(logString);
-      spy.returnValues[0].should.eql(result);
-      logger.fatal.restore();
-      done();
+    describe('error', (done) => {
+      it('successfully logs an \'error\' message', (done) => {
+        sampleMessage = generateLogOutput(logString, 'error', dlcId, backingServerId);
+        const spy = sinon.spy(logger, 'error');
+        logger.error(logString);
+        spy.getCall(0).args[0].should.eql(logString);
+        spy.returnValues[0].should.eql(sampleMessage);
+        logger.error.restore();
+        done();
+      });
+      it('writes the correct output to stdout', (done) => {
+        sampleMessage = generateLogOutput(logString, 'error', dlcId, backingServerId);
+        const inspect = stdout.inspect();
+        logger.error(logString);
+        inspect.restore();
+        inspect.output.should.eql([`${sampleMessage}`]);
+        done()
+      });
+    });
+    describe('fatal', (done) => {
+      it('successfully logs an \'fatal\' message', (done) => {
+        sampleMessage = generateLogOutput(logString, 'fatal', dlcId, backingServerId);
+        const spy = sinon.spy(logger, 'fatal');
+        logger.fatal(logString);
+        spy.getCall(0).args[0].should.eql(logString);
+        spy.returnValues[0].should.eql(sampleMessage);
+        logger.fatal.restore();
+        done();
+      });
+      it('writes the correct output to stdout', (done) => {
+        sampleMessage = generateLogOutput(logString, 'fatal', dlcId, backingServerId);
+        const inspect = stdout.inspect();
+        logger.fatal(logString);
+        inspect.restore();
+        inspect.output.should.eql([`${sampleMessage}`]);
+        done()
+      });
     });
   });
 });
