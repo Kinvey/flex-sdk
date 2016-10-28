45c4752ae2fb22093f293b6ee51e8575b88a3fec
Michael Salinger <mjsalinger@kinvey.com>
2016-10-28 06:58:24 -0400

    BACK-1991 Apply new branding and namespacing to sdk
    * Rename `kinvey-backend-sdk` to `kinvey-flex-sdk`
    * Rename `DataLink` to `FlexData`
    * Rename `BusinessLogic` to `FlexFunctions`
    * Change readme and examples to refer to the package as `sdk`, the method to generate the service as `service`, and the generated service as `flex`.

--- index.js
+++ index.js
@@ -10,4 +10,4 @@
 // or implied. See the License for the specific language governing permissions and limitations under
 // the License.
 
-module.exports = require('./lib/sdk');
+module.exports = require('./lib/flex');
